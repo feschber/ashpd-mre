154401e5d0a052de974872a9e4bb62b5b8f3aef9
Ferdinand Schober <ferdinand.schober@fau.de>
2024-01-03 23:02:33 +0100

    test fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ashpd::desktop::{input_capture::InputCapture, remote_desktop::RemoteDesktop};
+use ashpd::{desktop::{input_capture::{InputCapture, Capabilities}, remote_desktop::{RemoteDesktop, DeviceType}}, WindowIdentifier};
 
 #[tokio::main(flavor="current_thread")]
 async fn main() {
@@ -6,9 +6,43 @@ async fn main() {
 }
 
 async fn ic() {
-    let _ = InputCapture::new().await.unwrap();
+    let input_capture = InputCapture::new()
+        .await
+        .unwrap();
+    let (session, _) = input_capture
+        .create_session(
+            &ashpd::WindowIdentifier::default(),
+            (Capabilities::Pointer | Capabilities::Keyboard).into()
+        )
+        .await
+        .unwrap();
+    input_capture
+        .connect_to_eis(&session)
+        .await
+        .unwrap();
 }
 
 async fn rdp() {
-    let _ = RemoteDesktop::new().await.unwrap();
+    let remote_desktop = RemoteDesktop::new().await.unwrap();
+    let session = remote_desktop.create_session().await.unwrap();
+
+    remote_desktop
+        .select_devices(
+            &session,
+            DeviceType::Pointer | DeviceType::Keyboard | DeviceType::Touchscreen,
+        )
+        .await
+        .unwrap();
+
+    remote_desktop
+        .start(&session, &WindowIdentifier::default())
+        .await
+        .unwrap()
+        .response()
+        .unwrap();
+
+    remote_desktop
+        .connect_to_eis(&session)
+        .await
+        .unwrap();
 }
